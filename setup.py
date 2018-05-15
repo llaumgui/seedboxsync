baa481fee517f2e6fc940713488cc4334029b7f7
Guillaume Kulakowski <guillaume@kulakowski.fr>
2018-05-15 20:25:17 +0200

    Welcom 2018 !

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2017 Guillaume Kulakowski <guillaume@kulakowski.fr>
+# Copyright (C) 2015-2018 Guillaume Kulakowski <guillaume@kulakowski.fr>
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
