7bee30b568f593c0180a329f3290072c48f728a5
Guillaume Kulakowski <guillaume@kulakowski.fr>
2018-05-19 14:43:06 +0200

    Bump 2.0.0 finale !

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
     ],
 
     # What does your project relate to?
