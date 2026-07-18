c7d678aec845e968b98559c37278855b617410c5
Guillaume Kulakowski <guillaume@kulakowski.fr>
2026-07-18 16:15:04 +0200

    ✨ Support for custom sync time #93

--- seedboxsync/core/taskmanager/task/task_sync_blackhole.py
+++ seedboxsync/core/taskmanager/task/task_sync_blackhole.py
@@ -5,15 +5,17 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 #
+import os
 from huey import crontab
 from seedboxsync.core import current_app as app
 from seedboxsync.core.sync.services import blackhole as blackhole_service, BLACKHOLE_LOCK_NAME as LOCK_NAME
 
 task_manager = app.task_manager
 ctx = app.app_context()
+minute = os.getenv("SYNC_BLACKHOLE_MINUTE", "*")
 
 
-@task_manager.periodic_task(crontab(minute="*"))  # type: ignore[untyped-decorator]
+@task_manager.periodic_task(crontab(minute=minute))  # type: ignore[untyped-decorator]
 @task_manager.lock_task(LOCK_NAME)  # type: ignore[untyped-decorator]
 def periodic_sync_blackhole() -> None:
     with ctx:
--- seedboxsync/core/taskmanager/task/task_sync_seedbox.py
+++ seedboxsync/core/taskmanager/task/task_sync_seedbox.py
@@ -5,17 +5,18 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 #
+import os
 from huey import crontab
 from seedboxsync.core import current_app as app
 from seedboxsync.core.sync.services import seedbox as seedbox_service, SEEDBOX_LOCK_NAME as LOCK_NAME
 
 task_manager = app.task_manager
 ctx = app.app_context()
+minute = os.getenv("SYNC_SEEDBOX_MINUTE", "*/15")
 
 
 @task_manager.periodic_task(crontab(minute="*/15"))  # type: ignore[untyped-decorator]
 @task_manager.lock_task(LOCK_NAME)  # type: ignore[untyped-decorator]
 def periodic_sync_seedbox() -> None:
     with ctx:
-        app.logger.info("This task runs every minute")
         seedbox_service(False, True, False)
